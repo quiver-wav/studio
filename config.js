ec769e8cfafbdaf58f021f5713d7c3cb6d7423ae
Gabriele <161452478+quiver-wav@users.noreply.github.com>
2026-06-10 14:38:29 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -7,6 +7,6 @@
    Istruzioni complete nel README.md.
    ============================================================ */
 window.STUDIO_CONFIG = {
-  supabaseUrl: "https://vufjjorudtmlisecmfpn.supabase.co/rest/v1/",   
+  supabaseUrl: "https://vufjjorudtmlisecmfpn.supabase.co",   
   supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ1Zmpqb3J1ZHRtbGlzZWNtZnBuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwODg5OTgsImV4cCI6MjA5NjY2NDk5OH0.HviZahPGpiw_cU9Il7KC_Q4wj7339crO6ZL6Evuez6Y",
 };
